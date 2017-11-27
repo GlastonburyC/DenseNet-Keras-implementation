dfc60971790973c9afbb528e8e63dee7a5ed2e87
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-11-27 14:37:31 +0000

    Update dilationNet.py

--- dilationNet.py
+++ dilationNet.py
@@ -55,7 +55,7 @@ k = 12
 inputs = Input(shape=x_train.shape[1:])
 
 x = ZeroPadding2D((3, 3), name='conv1_zeropadding')(inputs)
-x = Conv2D(64,(7,7),padding='same', dilation_rate = 1,name='init_conv')(x)
+x = Conv2D(32,(3,3),padding='same', dilation_rate = 1,name='init_conv')(x)
 x = MaxPooling2D((2,2))(x)
 
 dense_lst = DenseBlock(x,no_layers=6,stage=1,feature_size=32,k=k)
@@ -78,32 +78,34 @@ model = Model(inputs=inputs, outputs=predictions)
 
 model.summary()
 
-opt = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
+opt = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
 model.compile(optimizer=opt,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-epochs=200
-datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+epochs=400
 
+# datagen = ImageDataGenerator(
+#         featurewise_center=False,  # set input mean to 0 over the dataset
+#         samplewise_center=False,  # set each sample mean to 0
+#         featurewise_std_normalization=False,  # divide inputs by std of the dataset
+#         samplewise_std_normalization=False,  # divide each input by its std
+#         zca_whitening=False,  # apply ZCA whitening
+#         rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+#         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+#         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+#         horizontal_flip=True,  # randomly flip images
+#         vertical_flip=False)  # randomly flip images
 
-datagen.fit(x_train)
-
-
-model.fit_generator(datagen.flow(x_train, y_train,
-                                     batch_size=128),
-                        steps_per_epoch=int(np.ceil(x_train.shape[0] / float(128))),
+model.fit(x_train, y_train,
+                         batch_size=128,
                         epochs=epochs,
-                        validation_data=(x_test, y_test),
-                        workers=8)
+                        validation_data=(x_test, y_test))
+
+# model.fit_generator(datagen.flow(x_train, y_train,
+#                                      batch_size=128),
+#                         steps_per_epoch=int(np.ceil(x_train.shape[0] / float(128))),
+#                         epochs=epochs,
+#                         validation_data=(x_test, y_test),
+#                         workers=8)
